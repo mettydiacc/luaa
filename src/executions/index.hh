9e9e3c04dd2ee705310fb59c764045874f0a0d6f
mettydiacc <mateidiaconu2007@icloud.com>
2024-03-22 16:12:40 +0100

    ..

--- src/executions/index.hh
+++ src/executions/index.hh
@@ -2,5 +2,10 @@
 
 inline void _index(int argc, char *argv[])
 {
-      std::cout << "Hello world!" << std::endl;
+      std::string filee = "lua ";
+      filee.append(argv[1]);
+      const char *file = filee.c_str();
+      int res = system(file);
+      std::cout << res << std::endl;
+      // i don't remember what i wanted to do lmao
 }
--- src/top/includes.cc
+++ src/top/includes.cc
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <vector>
 #include <filesystem>
+#include <fstream>
 
 extern "C"
 {
